6fd1d2ec71f3ad0d1898999833afa872a5f9856b
Alumno18 J601 <a18j601@campustls.local>
2018-04-10 10:59:37 -0500

    suebiendo archivos

--- Altoke/app/src/main/java/com/spacex/altoke/MainActivity.kt
+++ Altoke/app/src/main/java/com/spacex/altoke/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.spacex.altoke
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    button12.setOnClickListener(){
+        var intent  = Intent(this,HOME::class.java)
+
+                startActivity(intent)
+
+        Toast.makeText(this, "hola mundo",Toast.LENGTH_SHORT).show()
+
+        }
     }
+
 }
